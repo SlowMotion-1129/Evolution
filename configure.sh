b3d158280a37f9924c77b2819cd8b03018ec5e2d
SlowMotion Evolution <121202328+SlowMotion-1129@users.noreply.github.com>
2023-01-14 22:47:56 +0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,3 +7,7 @@ sed -i 's/192.168.1.1/192.168.129.5/g' package/base-files/files/bin/config_gener
 # passwall
 echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+
+Add luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openwrt-openclash
+pushd package/openwrt-openclash/tools/po2lmo && make && sudo make install 2>/dev/null && popd
